765ffcbe3c9263c2164c494894928681418d9dd3
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2024-01-17 19:19:37 +0700

    first commit konfiguration

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      {/* Hello word dengan taildwind css ditengah */}
+      <div className="flex justify-center items-center h-screen">
+        <h1 className="text-5xl text-green-500">Hello World</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
