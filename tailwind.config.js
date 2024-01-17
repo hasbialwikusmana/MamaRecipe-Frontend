f90c9bd24c2561e0279479eb83fd19b37a1e679e
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2024-01-17 21:26:22 +0700

    update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "airbnb-cereal": ["Airbnb Cereal App", "sans-serif"],
+      },
+      colors: {
+        primary: "#EFC81A",
+      },
+    },
   },
   plugins: [],
 };
