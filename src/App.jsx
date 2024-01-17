5dc2c2df016f45da92fa43e1267e4da2a731d8c7
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2024-01-18 00:22:30 +0700

    link router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
+import Router from "./router";
+
 function App() {
   return (
     <>
-      {/* Hello word dengan taildwind css ditengah */}
-      <div className="flex justify-center items-center h-screen">
-        <h1 className="text-5xl text-green-500">Hello World</h1>
-      </div>
+      <Router />
     </>
   );
 }
