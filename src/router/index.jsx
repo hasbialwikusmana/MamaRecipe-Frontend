779281e799deb0dcf7620ee6cbfffbaef88c18de
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2024-01-19 04:04:34 +0700

    Update index.jsx

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -2,21 +2,20 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
-
-const index = () => {
+import RecipesDetail from "../pages/Recipes/RecipesDetail";
+const Router = () => {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" exact element={<Navigate to="/home" replace="true"></Navigate>} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/auth/login" element={<Login />} />
-          <Route path="/auth/register" element={<Register />} />
-          <Route path="*" element={<p>Page not found</p>} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<Navigate to="/home" replace="true"></Navigate>} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/auth/login" element={<Login />} />
+        <Route path="/auth/register" element={<Register />} />
+        <Route path="/recipes/:id" element={<RecipesDetail />} />
+        <Route path="*" element={<p>Page not found</p>} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
-export default index;
+export default Router;
