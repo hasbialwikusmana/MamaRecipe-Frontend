1aff90b08c7c08a48497d7ba9580195865b14722
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2024-01-24 22:41:22 +0700

    Update index.jsx

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,12 +1,58 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { FaBars } from "react-icons/fa";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 function Navbar() {
+  const token = localStorage.getItem("token");
+  const isLogin = token ? true : false;
+  const displayMenu = isLogin ? "flex" : "hidden";
   const [isMenuOpen, setMenuOpen] = useState(false);
+  const [users, setUsers] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const baseURL = import.meta.env.VITE_API_URL;
+    if (token) {
+      axios
+        .get(`${baseURL}/users/profile`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then((res) => {
+          setUsers(res.data.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, [token]);
 
   const handleHamburger = () => {
     setMenuOpen(!isMenuOpen);
+  };
+
+  const handleLogout = () => {
+    // Show SweetAlert2 confirmation dialog
+    Swal.fire({
+      title: "Logout",
+      text: "Are you sure you want to logout?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, logout!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // User confirmed, perform logout
+        localStorage.clear();
+        // You may add additional logout logic here
+        // Optionally, you can redirect to the login page or another page
+        navigate("/auth/login");
+      }
+    });
   };
 
   return (
@@ -16,10 +62,10 @@ function Navbar() {
           <NavLink to="/" className="text-gray-900 hover:text-primary">
             Home
           </NavLink>
-          <NavLink to="/recipe-add" className="text-gray-900 hover:text-primary">
+          <NavLink to="/recipes" className={`${displayMenu} text-gray-900 hover:text-primary`}>
             Add Recipes
           </NavLink>
-          <NavLink to="/profile" className="text-gray-900 hover:text-primary">
+          <NavLink to="/profile" className={`${displayMenu} text-gray-900 hover:text-primary`}>
             Profile
           </NavLink>
         </div>
@@ -36,26 +82,24 @@ function Navbar() {
               </NavLink>
             </li>
             <li className="group">
-              <NavLink to="/recipe-add" className="text-base text-gray-900 py-2 mx-8 group-hover:text-primary">
+              <NavLink to="/recipe-add" className={`${displayMenu}text-base text-gray-900 py-2 mx-8 group-hover:text-primary`}>
                 Add Recipe
               </NavLink>
             </li>
             <li className="group">
-              <NavLink to="/profile" className="text-base text-gray-900 py-2 mx-8 group-hover:text-primary">
+              <NavLink to="/profile" className={`${displayMenu}text-base text-gray-900 py-2 mx-8 group-hover:text-primary`}>
                 Profile
               </NavLink>
             </li>
           </ul>
         </nav>
 
-        <Link to="/auth/login" className="flex items-center gap-1 text-white">
-          <div>
-            <img src="/img/icon/User icon.svg" width="32" alt="icon-user" className="xl:w-10" />
-          </div>
+        <button onClick={handleLogout} className="flex items-center gap-1 text-white">
+          <div>{isLogin ? <img src={users.image} width="32" height="32" alt="icon-user" className="xl:w-10 rounded-full" /> : <img src="/img/icon/User icon.svg" width="32" alt="icon-user" className="xl:w-10" />}</div>
           {/* If in home text-white */}
 
-          <p className="text-white font-semibold text-sm">Login</p>
-        </Link>
+          <p className="text-white font-semibold text-sm">{isLogin ? "Logout" : "Login"}</p>
+        </button>
       </div>
     </header>
   );
