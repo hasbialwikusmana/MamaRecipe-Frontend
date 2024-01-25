ed247e06409dc6c1be1c5370d8d7f9c6f0a3a6df
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2024-01-25 22:11:54 +0700

    update navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { FaBars } from "react-icons/fa";
+import { FaBars, FaHome, FaBook, FaUser, FaSignOutAlt } from "react-icons/fa";
 import { NavLink, useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
 
@@ -9,6 +9,7 @@ function Navbar() {
   const isLogin = token ? true : false;
   const displayMenu = isLogin ? "flex" : "hidden";
   const [isMenuOpen, setMenuOpen] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [users, setUsers] = useState([]);
   const navigate = useNavigate();
 
@@ -34,27 +35,34 @@ function Navbar() {
     setMenuOpen(!isMenuOpen);
   };
 
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
   const handleLogout = () => {
-    // Show SweetAlert2 confirmation dialog
     Swal.fire({
       title: "Logout",
       text: "Are you sure you want to logout?",
       icon: "warning",
       showCancelButton: true,
-      confirmButtonColor: "#3085d6",
+      confirmButtonColor: "#EFC81A",
       cancelButtonColor: "#d33",
       confirmButtonText: "Yes, logout!",
     }).then((result) => {
       if (result.isConfirmed) {
-        // User confirmed, perform logout
-        localStorage.clear();
-        // You may add additional logout logic here
-        // Optionally, you can redirect to the login page or another page
+        localStorage.removeItem("token");
+        localStorage.removeItem("refreshToken");
         navigate("/auth/login");
       }
     });
+
+    setIsDropdownOpen(false);
   };
 
+  // const handleProfile = () => {
+  //   navigate("/profile");
+  //   setIsDropdownOpen(false);
+  // };
   return (
     <header className="w-full absolute top-0 left-0 z-50">
       <div className="flex items-center justify-between px-5 md:px-16 lg:px-24 h-24">
@@ -71,10 +79,10 @@ function Navbar() {
         </div>
 
         <button id="hamburger" name="hamburger" type="button" onClick={handleHamburger} className="block md:hidden">
-          <FaBars className="text-2xl text-gray-900 hover:text-primary" />
+          <FaBars className="text-2xl text-gray-900 hover:text-primary hidden" />
         </button>
 
-        <nav id="nav-menu" className={`md:hidden absolute py-5 bg-white border shadow-xl rounded-lg max-w-[250px] w-full left-8 top-20 z-10 ${isMenuOpen ? "" : "hidden"}`}>
+        {/* <div className={`md:hidden absolute py-5 bg-white border shadow-xl rounded-lg max-w-[250px] w-full left-8 top-20 z-10 ${isMenuOpen ? "" : "hidden"}`}>
           <ul className="block">
             <li className="group">
               <NavLink to="/" className="flex text-base text-gray-900 py-2 mx-8 group-hover:text-primary">
@@ -92,14 +100,51 @@ function Navbar() {
               </NavLink>
             </li>
           </ul>
-        </nav>
+        </div> */}
 
-        <button onClick={handleLogout} className="flex items-center gap-1 text-white">
-          <div>{isLogin ? <img src={users.image} width="32" height="32" alt="icon-user" className="xl:w-10 rounded-full" /> : <img src="/img/icon/User icon.svg" width="32" alt="icon-user" className="xl:w-10" />}</div>
-          {/* If in home text-white */}
-
-          <p className="text-white font-semibold text-sm">{isLogin ? "Logout" : "Login"}</p>
-        </button>
+        <div style={{ position: "relative" }}>
+          <div onClick={toggleDropdown}>
+            {isLogin ? (
+              users.image ? (
+                <img src={users.image} alt="icon-user" className="xl:w-12 rounded-full cursor-pointer" />
+              ) : (
+                <img src="/img/icon/User icon.svg" alt="icon-user" className="xl:w-12 cursor-pointer" />
+              )
+            ) : (
+              <img src="/img/icon/User icon.svg" alt="icon-user" className="xl:w-12 cursor-pointer" />
+            )}
+          </div>
+          {isDropdownOpen && (
+            <div className="absolute mt-2 bg-white rounded-md shadow-lg right-0 w-48 ">
+              <ul className="block">
+                <li className="group xl:hidden lg:hidden md:hidden">
+                  <NavLink to="/" className="flex items-center py-3 px-6 cursor-pointer hover:text-primary ">
+                    <FaHome className="mr-2" />
+                    Home
+                  </NavLink>
+                </li>
+                <li className="group xl:hidden lg:hidden  md:hidden">
+                  <NavLink to="/recipe-add" className="flex items-center py-3 px-6 cursor-pointer hover:text-primary">
+                    <FaBook className="mr-2" />
+                    Add Recipe
+                  </NavLink>
+                </li>
+                <li className="group xl:hidden lg:hidden md:hidden">
+                  <NavLink to="/profile" className="flex items-center py-3 px-6 cursor-pointer hover:text-primary">
+                    <FaUser className="mr-2" />
+                    Profile
+                  </NavLink>
+                </li>
+                <li className="group">
+                  <div className="flex items-center py-3 px-6 cursor-pointer hover:text-primary" onClick={handleLogout}>
+                    <FaSignOutAlt className="mr-2" />
+                    Logout
+                  </div>
+                </li>
+              </ul>
+            </div>
+          )}
+        </div>
       </div>
     </header>
   );
