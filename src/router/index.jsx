215b323633e229ca3f7dff8cdea428c0bb5d2ad5
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2024-01-30 12:50:54 +0700

    update router

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,22 +1,110 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PrivateRoute from "./Protect";
+
+// Components
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+// Main
 import Home from "../pages/Home";
-import Login from "../pages/Login";
-import Register from "../pages/Register";
-import RecipesDetail from "../pages/Recipes/RecipesDetail";
 import NotFound from "../pages/NotFound";
-const Router = () => {
+
+// Auth
+import Login from "../pages/Auth/Login";
+import Register from "../pages/Auth/Register";
+
+// Recipe
+import Add from "../pages/Recipes/AddRecipes";
+import Update from "../pages/Recipes/UpdateRecipes";
+import Detail from "../pages/Recipes/RecipesDetail";
+import List from "../pages/Recipes/ListRecipes";
+import Video from "../pages/Recipes/VideoRecipes";
+
+// User
+import Profile from "../pages/Users/Profile";
+
+function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" exact element={<Navigate to="/home" replace="true"></Navigate>} />
-        <Route path="/home" element={<Home />} />
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <Navbar />
+              <Home />
+              <Footer />
+            </PrivateRoute>
+          }
+        ></Route>
+
         <Route path="/auth/login" element={<Login />} />
         <Route path="/auth/register" element={<Register />} />
-        <Route path="/recipes/:id" element={<RecipesDetail />} />
+
+        <Route
+          path="/recipes/add"
+          element={
+            <PrivateRoute>
+              <Navbar />
+              <Add />
+              <Footer />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="recipes/update/:id"
+          element={
+            <PrivateRoute>
+              <Navbar />
+              <Update />
+              <Footer />
+            </PrivateRoute>
+          }
+        />
+
+        <Route
+          path="/recipes/detail/:id"
+          element={
+            <PrivateRoute>
+              <Detail />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/recipes/list"
+          element={
+            <PrivateRoute>
+              <Navbar />
+              <List />
+              <Footer />
+            </PrivateRoute>
+          }
+        ></Route>
+        <Route
+          path="recipes/video/:id"
+          element={
+            <PrivateRoute>
+              <Navbar />
+              <Video />
+            </PrivateRoute>
+          }
+        />
+
+        <Route
+          path="/profile"
+          element={
+            <PrivateRoute>
+              <Navbar />
+              <Profile />
+              <Footer />
+            </PrivateRoute>
+          }
+        />
+
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
-};
+}
 
 export default Router;
